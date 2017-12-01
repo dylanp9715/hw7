92d55491ecdc26a7f0e4d9e42f97cd9f8a11dae4
Dylan J. Park <dpark09@lab118t.eecs.tufts.edu>
2017-12-01 12:24:57 -0500

    new segments

--- segments.c
+++ segments.c
@@ -12,8 +12,11 @@
 
 void check_openID(Segments *s);
 void initialize_segments(Segments *s);
-int next_open_ID(Segments *s);
+//int next_open_ID(Segments *s);
 void unmap_all_segments(Segments *s);
+//void expand(Segments *s);
+//int seg_size = 0;
+//int seg_capacity = 5;
 
 /* Purpose: Maps a segment to the next open spot in memory with a size of given
  * number of words
@@ -24,22 +27,56 @@ void unmap_all_segments(Segments *s);
 uint32_t map_segment(Segments *s, int num_words) {
         check_openID(s);
         int new_ID = 0;
-
+        //printf("Num words: %d\n", num_words);
+        // if (seg_size >= seg_capacity - 1){
+        //         fprintf(stderr, "IN EXPAND\n");
+        //         expand(&segments);
+        // }
+        //printf("seg_size: %d\n", seg_size);
         if (Seq_length(s->openID) == 0) {
                 new_ID = Seq_length(s->segments);
-                Seq_addhi(s->segments, UArray_new(num_words, 
-                                                  sizeof(uint32_t)));
+                //new_ID = seg_size;
+                //printf("here\n");
+                uint32_t *segment = calloc((num_words + 1), sizeof(uint32_t *));
+                segment[0] = num_words;
+                //}
+                //else {
+                //        segment = malloc(sizeof(uint32_t) * (num_words));
+                //}
+                //printf("new_ID: %d\n", new_ID);
+                //fprintf(stderr, "NewID = %d segments size = %d\n", new_ID, seg_size);
+                //segments[new_ID] = segment;
+                Seq_addhi(s->segments, segment);
+                //seg_size++;
                 return new_ID;
         }
         else {
                 int *reused_ID = (int *)Seq_remlo(s->openID);
-                Seq_put(s->segments, *reused_ID, UArray_new(num_words, 
-                         sizeof(uint32_t)));
+                uint32_t *segment = calloc((num_words + 1), sizeof(uint32_t *));
+                segment[0] = num_words;
+                //}
+                //else {
+                //        segment = malloc(sizeof(uint32_t) * (num_words));
+                // }
+                Seq_put(s->segments, *reused_ID, segment);
+                //segments[*reused_ID] = segment;
                 new_ID = *reused_ID;
                 free(reused_ID);
+                //seg_size++;
                 return new_ID;
         }
 }
+
+//void expand(uint32_t ***segments){
+//        *segments = realloc(*segments, (seg_capacity * 2 + 1) * sizeof(uint32_t *));
+        /*uint32_t **new_array = calloc((seg_capacity * 2 + 1), sizeof(uint32_t *));
+        for (int i = 0; i < seg_size; ++i){
+                new_array[i] = *segments[i];
+        }
+        free(*segments);
+        *segments = new_array;*/
+//        seg_capacity = (seg_capacity * 2) + 1;
+//}
 
 /* Purpose: Checks to see if the openID's sequence has been initialized yet
  * Arguments: Takes in a Segments Struct with segments and next open id's
@@ -67,12 +104,18 @@ void initialize_segments(Segments *s) {
  * Returns: Nothing it is a void function
  */
 void unmap_segment(Segments *s, int unmapID) {
+        //printf("here2\n");
         int *new_ID = malloc(sizeof(int));
-        UArray_T temp = (UArray_T)Seq_get(s->segments, unmapID);
-        UArray_free(&temp);
+        //UArray_T temp = (UArray_T)Seq_get(s->segments, unmapID);
+        //UArray_free(&temp);
+        uint32_t *temp = (uint32_t *)Seq_get(s->segments, unmapID);
+        //uint32_t *temp = segments[unmapID];
+        free(temp);
         Seq_put(s->segments, unmapID, NULL);
         *new_ID = unmapID;
         Seq_addhi(s->openID, new_ID);
+        //seg_size--;
+        //printf("seg_size: %d\n", seg_size);
 }
 
 /* Purpose: Stores a word in a specified segment at a given offset
@@ -82,8 +125,21 @@ void unmap_segment(Segments *s, int unmapID) {
  */
 void segment_store(Segments *s, int segmentID, int offset, uint32_t word) {
 
-        UArray_T temp = (UArray_T)Seq_get(s->segments, segmentID);
-        *(uint32_t *)UArray_at(temp, offset) = word;
+        //UArray_T temp = (UArray_T)Seq_get(s->segments, segmentID);
+        uint32_t *temp = (uint32_t *)Seq_get(s->segments, segmentID);
+        //fprintf(stderr, "SEGMENT ID IS = %d\n", segmentID); 
+        //uint32_t *temp = segments[segmentID];
+        //*(uint32_t *)UArray_at(temp, offset) = word;
+        //if (segmentID != 0) {
+        //printf("offset, word: %d, %x\n", offset, word);
+        //printf("here\n");
+        temp[offset] = word;
+
+
+        //} else {
+                //printf("offset, word: %d, %d\n", offset, word);
+                //temp[offset] = word;
+        //}
 }
 
 /* Purpose: Loads a word from a specified segment at a given offset
@@ -92,8 +148,18 @@ void segment_store(Segments *s, int segmentID, int offset, uint32_t word) {
  * Returns: The word at the segments given offset to be loaded into a register
  */
 uint32_t segment_load(Segments *s, int segmentID, int offset) {
-        UArray_T temp = Seq_get(s->segments, segmentID);
-        uint32_t word = *(uint32_t *)UArray_at(temp, offset);
+        //UArray_T temp = Seq_get(s->segments, segmentID);
+        uint32_t *temp = (uint32_t *)Seq_get(s->segments, segmentID);
+        //printf("segID: %d\n", word);
+        //uint32_t *temp = segments[segmentID];
+        //uint32_t word;
+        //if (segmentID != 0) {
+                //printf("here\n");
+        uint32_t word = temp[offset];
+        //printf("seg_size: %d\n", seg_size);
+        //} else {
+        //        word = temp[offset];
+        //}
         return word;
 }
 
@@ -104,24 +170,26 @@ uint32_t segment_load(Segments *s, int segmentID, int offset) {
  */
 void free_all_segments(Segments *s) {
 
-        int seg_length = Seq_length(s->segments);
+        int seg_length = Seq_length(s->segments);//seg_size;//Seq_length(s->segments);
         int id_length = Seq_length(s->openID);
 
         for (int i = 0; i < seg_length; ++i) {
 
                 if (Seq_get(s->segments, i) != NULL){
 
-                        UArray_T temp = (UArray_T)Seq_get(s->segments, i);
-                        UArray_free(&temp);
+                        //UArray_T temp = (UArray_T)Seq_get(s->segments, i);
+                        uint32_t *temp = (uint32_t *)Seq_get(s->segments, i);//segments[i];//(uint32_t *)Seq_get(s->segments, i);
+                        //UArray_free(&temp);
+                        free(temp);
 
                 }
 
         }
         for (int j = 0; j < id_length; j++){
                 int *temp = Seq_get(s->openID, j);
-
                 free(temp);
         }
         Seq_free(&(s->openID));
         Seq_free(&(s->segments));
+        //free(segments);
 }
