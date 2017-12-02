dc3ce0cd78e3ccdfd6f5256adac9aa932d5ed877
Dylan J. Park <dpark09@lab118t.eecs.tufts.edu>
2017-12-01 19:29:20 -0500

    new segments.h

--- segments.h
+++ segments.h
@@ -12,19 +12,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
-#include "uarray.h"
+//#include "uarray.h"
 #include "seq.h"
 
-typedef struct {
-        Seq_T openID;
-        Seq_T segments;
-} Segments;
+// typedef struct {
+//         Seq_T openID;
+//         Seq_T segments;
+// } Segments;
 
+Seq_T openID;
 
-uint32_t map_segment(Segments *s, int num_words);
-void unmap_segment(Segments *s, int unmapID);
-void segment_store(Segments *s, int segmentID, int offset, uint32_t word);
-uint32_t segment_load(Segments *s, int segmentID, int offset);
-void free_all_segments(Segments *s);
+uint32_t map_segment(uint32_t **segments, int num_words, int seg_size);
+void unmap_segment(uint32_t **segments, int unmapID);
+void segment_store(uint32_t **segments, int segmentID, int offset, uint32_t word);
+uint32_t segment_load(uint32_t **segments, int segmentID, int offset);
+void free_all_segments(uint32_t **segments, int seg_capacity);
+int get_size(int index);
+void set_size(int index, int size);
 
 #endif
